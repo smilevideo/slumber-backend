cf6c68bd51c9c4e8a460f269c40b7a7d1cebc697
andrewkim <andrewk1216@gmail.com>
2019-11-21 14:46:04 -0500

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create]
+  get '/profile', to: 'users#profile'
+
+  post '/login', to: 'auth#create'
 end
